5df2cdf18e7067f09300ad3f2ad7e6546122dd24
ibktommy <atomquartz@gmail.com>
2023-12-24 11:55:14 +0100

    Add Data to Firestore-db

--- src/FirebaseCrudApp/FirebaseCrudApp.jsx
+++ src/FirebaseCrudApp/FirebaseCrudApp.jsx
@@ -1,40 +1,66 @@
-import { useState } from 'react'
-import { db } from './firebase-config'
-import { collection, getDocs } from 'firebase/firestore'
-import { useEffect } from 'react'
+import { useState } from 'react';
+import { db } from './firebase-config';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
+import { useEffect } from 'react';
 
 const FirebaseCrudApp = () => {
-  const [users, setUsers] = useState([])
-  const usersCollectionRef = collection(db, "users")
+	const [userName, setUserName] = useState('');
+	const [userAge, setUserAge] = useState('');
 
-  // Use the useEffect-hook to get data from the api when the app re-renders except the data does not change
-  useEffect(() => {
-    // Function to get Users from database
-    const getUsers = async () => {
-      const usersData = await getDocs(usersCollectionRef)
-      setUsers(usersData.docs.map((usersData) => ({...usersData.data(), id: usersData.id})))
-    }
+	const [users, setUsers] = useState([]);
+	const usersCollectionRef = collection(db, 'users');
 
-    getUsers()
-  }, [])
+	// Use the useEffect-hook to get data from the api when the app re-renders except the data does not change
+	useEffect(() => {
+		// Function to get Users from database
+		const getUsers = async () => {
+			const usersData = await getDocs(usersCollectionRef);
+			setUsers(
+				usersData.docs.map((usersData) => ({
+					...usersData.data(),
+					id: usersData.id,
+				})),
+			);
+		};
 
-  console.log(users);
+		getUsers();
+	}, []);
 
-  return (
-    <div>
-      {
-        users.map((user) => {
-          const {name, age, id} = user
-          return (
-            <div key={id}>
-              <h1>Name: {name}</h1>
-              <h1>Name: {age}</h1>
-            </div>
-          )
-        })
-      }
-    </div>
-  )
-}
+	// console.log(users);
 
-export default FirebaseCrudApp
+	// Function that gets called when we create user
+	const createUserHandler = async () => {
+    await addDoc(usersCollectionRef, {name: userName, age: userAge})
+
+    setUserAge("")
+    setUserName("")
+  };
+
+  
+	return (
+		<div>
+			<input
+				type='text'
+				placeholder='Enter your name'
+				onChange={(e) => setUserName(e.target.value)}
+			/>
+			<input
+				type='number'
+				placeholder='Enter your Age'
+				onChange={(e) => setUserAge(e.target.value)}
+			/>
+			<button onClick={createUserHandler}>Create User</button>
+			{users.map((user) => {
+				const { name, age, id } = user;
+				return (
+					<div key={id}>
+						<h1>Name: {name}</h1>
+						<h1>Age: {age}</h1>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
+
+export default FirebaseCrudApp;
